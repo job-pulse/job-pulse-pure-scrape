4100d2245440b9bf44cd9ddcda449268737e40e1
jeee <45247795+je888@users.noreply.github.com>
2023-08-10 18:37:46 -0400

    handle uncaight exception in logging

--- src/util/logger.py
+++ src/util/logger.py
@@ -1,4 +1,10 @@
+import sys
 import logging
+
+def global_exception_handler(type, value, tb):
+    logging.error("Uncaught exception", exc_info=(type, value, tb))
+
+sys.excepthook = global_exception_handler
 
 def setup_logging(log_filename, log_format='%(asctime)s %(levelname)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p'):
     # Check if logging is already configured
@@ -8,7 +14,6 @@ def setup_logging(log_filename, log_format='%(asctime)s %(levelname)s %(message)
 
         # Set up logging to the terminal
         stream_handler = logging.StreamHandler()
-
         formatter = logging.Formatter('%(levelname)s %(message)s', datefmt=datefmt)
         stream_handler.setFormatter(formatter)
         logging.getLogger().addHandler(stream_handler)
