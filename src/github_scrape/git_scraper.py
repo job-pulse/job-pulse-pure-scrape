d05a2643ba73781a25c830af5a9760020ed1e53c
jeee <45247795+je888@users.noreply.github.com>
2023-09-06 22:42:51 -0400

    add git scrape to main

---
+++ src/github_scrape/git_scraper.py
@@ -0,0 +1,18 @@
+from util.getGitRepoPostings import fetch_and_save_data
+from util.firebase_handler import FirebaseHandler
+
+from util.constants import ng_us_urls, intern_us_urls, \
+  ng_us_output_filename, intern_us_output_filename
+
+def run_git_scrape():
+    ng_us_postings = fetch_and_save_data(ng_us_urls, ng_us_output_filename, "USA")
+    intern_us_postings = fetch_and_save_data(intern_us_urls, intern_us_output_filename, "USA")
+
+
+    firebase_handler = FirebaseHandler()
+
+    for data in ng_us_postings:
+        firebase_handler.add_job(data, 'all_companies')
+
+    for data in intern_us_postings:
+        firebase_handler.add_job(data, 'all_companies') 
--- src/main.py
+++ src/main.py
@@ -1,17 +1,22 @@
 import argparse
 from pure_scrape.pure_scrape_runner import run_pure_scrape
+from github_scrape.git_scraper import run_git_scrape
 
 # python3 src/main.py --run_pure
 
-def main(run_pure, prod):
+def main(run_pure, prod, run_git):
     if run_pure:
         run_pure_scrape(prod=prod)
+    if run_git:
+        run_git_scrape()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Script to control the running of the various scrapers")
     parser.add_argument("--run_pure", action="store_true", help="Run pure scrape")
     parser.add_argument("--prod", action="store_true", help="Run pure scrape")
+    parser.add_argument("--run_git", action="store_true", help="Run git scrape")
+
 
     args = parser.parse_args()
-    main(args.run_pure, args.prod)
+    main(args.run_pure, args.prod, args.run_git)
 
